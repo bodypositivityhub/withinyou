36dc1f4a1659f7d69f2c0e27f9ab7c07998fce12
bodypositivtyhub <vihaan.chereddi@akahyd.org>
2026-03-22 22:05:06 +0530

    update project

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/withinyou/'
+  base: '/withinyou/',
 
   server: {
     host: "::",
